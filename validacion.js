b331e916ea9f14674f2c94931d85e376c4165ee6
juancho-te <barriajuanalberto@gmail.com>
2022-06-28 08:37:49 -0300

    Se quito seccion javascript index.html y se agrego al va alrchivo validacion.js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,10 @@
 //Haz tú validación en javascript acá
+
+        //alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height);
+     function valida (email){
+        let emailText=email;
+        if (emailText.indexOf(".com") == -1)
+        alert("error"+emailText);
+        boton.setFocus();
+
+    }
